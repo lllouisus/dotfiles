db10571075d59f01489c3a009a97dd9a15be9ebe
louisrc3ys <liyinghua7797@gmail.com>
2022-10-30 14:53:04 +0800

    Update

---
+++ scripts/feh.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+while true; do
+
+	feh --bg-fill --no-fehbg --randomize ~/Gentoo_Config/wallpaper
+
+	sleep 15
+
+done
---
+++ scripts/static-feh.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+feh --bg-fill ~/Gentoo_Config/wallpaper/55.png 
