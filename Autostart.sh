41e901ac096d9845334fccb4d67a10e3e20dc6ea
louisrc3ys <liyinghua7797@gmail.com>
2022-11-01 20:13:24 +0800

    Add: Update Battery

--- Autostart.sh
+++ Autostart.sh
@@ -6,7 +6,8 @@ $HOME/.screenlayout/display.sh
 # # # gentoo-pipewire-launcher &
 pgrep -x clipit > /dev/null || clipit &
 pgrep -x fcitx > /dev/null || fcitx &
-# pgrep -x flameshot > /dev/null || flameshot &
+pgrep -x flameshot > /dev/null || flameshot &
+pgrep -x xfce4-power-manager > /dev/null || xfce4-power-manager &
 xsetroot -cursor_name left_ptr
 nitrogen --restore &
 xset dpms 600 600 600
