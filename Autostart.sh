df7b1a3b0d8788c59264197fa81b816444bbd2c0
xanmodlouis <xanmodlouis@gmail.com>
2023-04-16 13:14:38 +0800

    update

--- Autostart.sh
+++ Autostart.sh
@@ -2,13 +2,12 @@
 #
 picom --experimental-backends --config ~/.config/picom.conf &
 
-$HOME/.screenlayout/display.sh
 # # # gentoo-pipewire-launcher &
-pgrep -x clipit > /dev/null || clipit &
-pgrep -x fcitx > /dev/null || fcitx &
+pgrep -x copyq > /dev/null || copyq &
+pgrep -x fcitx5 > /dev/null || fcitx5 &
 pgrep -x flameshot > /dev/null || flameshot &
 pgrep -x xfce4-power-manager > /dev/null || xfce4-power-manager &
-xsetroot -cursor_name left_ptr
+# xsetroot -cursor_name left_ptr
 nitrogen --restore &
-xset dpms 600 600 600
+# xset dpms 600 600 600
 
